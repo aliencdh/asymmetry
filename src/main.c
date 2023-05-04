bdcbed25cf6773f88486775f05e78955f70bdb79
aliencdh <cdevhax@gmail.com>
2023-05-04 14:25:20 +0200

    implement cells

--- src/main.c
+++ src/main.c
@@ -2,9 +2,31 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define WINDOW_SIZE 1000
+#define CELL_SIZE 15
+#define CELL_BORDER_WIDTH 2
+#define CELL_COUNT WINDOW_SIZE / CELL_SIZE
+
+typedef unsigned int Cell;
+
+typedef struct {
+  unsigned char r, g, b, a;
+} Rgba;
+
+void cell_draw(Cell cell, size_t i, Rgba *colors, SDL_Renderer *renderer) {
+  Rgba clr = colors[cell];
+  SDL_SetRenderDrawColor(renderer, clr.r, clr.g, clr.b, clr.a);
+  SDL_Rect rect = {.x = i * CELL_SIZE + CELL_BORDER_WIDTH,
+                   .y = i * CELL_SIZE + CELL_BORDER_WIDTH,
+                   .w = CELL_SIZE - CELL_BORDER_WIDTH,
+                   .h = CELL_SIZE - CELL_BORDER_WIDTH};
+  SDL_RenderFillRect(renderer, &rect);
+}
 
 int main(void) {
   // init
@@ -24,6 +46,23 @@ int main(void) {
     return 2;
   }
 
+  // data
+  Cell cells[CELL_COUNT];
+  memset(cells, 0, sizeof(cells));
+
+  Rgba colors[2];
+  colors[0].r = 0;
+  colors[0].g = 0;
+  colors[0].b = 0;
+  colors[0].a = 255;
+  colors[1].r = 255;
+  colors[1].g = 255;
+  colors[1].b = 255;
+  colors[1].a = 255;
+
+  cells[0] = 1;
+
+  // main loop
   for (;;) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -37,6 +76,11 @@ int main(void) {
     // clear the screen
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    // draw cells
+    for (size_t i = 0; i < CELL_COUNT; i++) {
+      cell_draw(cells[i], i, colors, renderer);
+    }
 
     SDL_RenderPresent(renderer);
   }
---
+++ src/transition-table.h
@@ -0,0 +1,8 @@
+#ifndef TRANSITION_TABLE_H /* TRANSITION_TABLE_H */
+#define TRANSITION_TABLE_H
+
+typedef struct {
+
+} StateTransitions;
+
+#endif /* TRANSITION_TABLE_H */
